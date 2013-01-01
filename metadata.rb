a652f75e2db5d065886392b07fc2f9458558ce35
Adam Duro <adam@duromedia.com>
2013-01-01 15:44:42 -0800

    Adding version 0.1.0

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "base"
+maintainer       "ZehnerGroup"
+maintainer_email "aduro@zehnergroup.com"
+license          "All rights reserved"
+description      "Installs/Configures a generic base server profile"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+depends "htop"
---
+++ recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: base
+# Recipe:: default
+#
+# Copyright (C) 2012 Adam Duro
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+# Install htop
+package "htop" do
+  action :install
+end
