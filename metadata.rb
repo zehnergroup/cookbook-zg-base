847fd18a2ececdf47289d0f85abe5acdcab7d85b
Adam Duro <adam@duromedia.com>
2013-04-20 17:45:09 -0700

    Cleaned up cruft, made version numbers explicit and fixed, version bump

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,7 @@ maintainer_email "aduro@zehnergroup.com"
 license          "All rights reserved"
 description      "Installs/Configures a generic base server profile"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 
-depends "apt", "~> 1.8.4"
-depends "git", "~> 2.1.4"
-depends "firewall", "~> 0.10.2"
-depends "hostsfile", "~> 0.2.5"
+depends "apt",        "= 1.8.4"
+depends "firewall",   "= 0.10.2"
